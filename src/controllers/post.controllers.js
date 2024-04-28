22ef3e876a6732251bafc4d3ce14090459045b3e
Ranjan1501 <ranjankumar1757@gmail.com>
2024-04-28 18:43:06 +0530

    WIP- Recommendaion api Backend

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -3,7 +3,7 @@ const pool = require("../config/db");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/posts", async (req, res) => {
   try {
     const data = await pool.query("SELECT * FROM posts");
     res.json(data.rows);
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:userId", async (req, res) => {
+router.get("/posts/:userId", async (req, res) => {
   const { userId } = req.params;
   console.log("userId: ", userId);
   try {
@@ -28,7 +28,50 @@ router.get("/:userId", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+// recommendation api
+router.get("/recommendation/posts/:userId", async (req, res) => {
+  const { userId } = req.params;
+  console.log("userId: ", userId);
+  try {
+    // calculate popularity score based on like and comments
+    const totalLikesByUser = await pool.query(
+      "SELECT COUNT(*) FROM likes WHERE user_id = $1",
+      [userId]
+    );
+    const likes = totalLikesByUser.rows[0].count;
+    console.log("totalLikesByUser: ", likes);
+
+    //  // calculate popularity score based on like and comments
+    const totalCommentByUser = await pool.query(
+      "SELECT COUNT(*) FROM comments WHERE user_id = $1",
+      [userId]
+    );
+    const comments = totalCommentByUser.rows[0].count;
+    console.log("totalCommentByUser: ", comments);
+
+    // get all followed user_id by this user
+    const follweredByUser = await pool.query(
+      "SELECT * FROM friendList WHERE follower_id = $1",
+      [userId]
+    );
+
+    const allFollowedUserId = follweredByUser.rows[0].friend_id;
+    console.log("follweredByUser: ", allFollowedUserId);
+
+    // get all post with user id
+    const data = await pool.query(
+      "SELECT * FROM posts WHERE published_by = $1",
+      [allFollowedUserId]
+    );
+
+    res.json(data.rows);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus({ status: 500, message: err.message });
+  }
+});
+
+router.post("/posts", async (req, res) => {
   let { name, age, email } = req.body;
   console.log("des: ", email);
   try {
